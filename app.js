c8ac5f61f1d0a477c6a2fa225419285ba5645f47
dogotom <tomasmm@msn.com>
2015-06-08 23:17:04 +0200

    Marco de la aplicacion

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 //[TMM] var users = require('./routes/users');
@@ -14,15 +15,18 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 // uncomment after placing your favicon in /public
+app.use(partials());
+
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
+
 //[TMM]app.use('/users', users);
 
 // catch 404 and forward to error handler
