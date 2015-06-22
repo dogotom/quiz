e7567dc97e80b971837914ba64635d521db127b8
dogotom <tomasmm@msn.com>
2015-06-22 19:14:40 +0200

    Con AutoLoad

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var router = express.Router();
+
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz TMM' });  //[TMM] este title va a verse en la vista index.ejs
+  res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
 
-router.get('/author',function(req,res) {
-	res.render('author',{ autor: 'Tomás Martín' });
-});
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+router.get('/author',function(req,res) {res.render('author',{ autor: 'Tomás Martín' });});
 
 module.exports = router;
